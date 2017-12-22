a15684dbb369fbf165544bf7d540556cfbc117c8
bachner0 <matt.cline.it@gmail.com>
2017-12-21 20:32:07 -0500

    finished player camera. input spell creation wizard and base class.

--- CasterConfineUnityProject/Assets/Scripts/PlayerScripts/CameraController.cs
+++ CasterConfineUnityProject/Assets/Scripts/PlayerScripts/CameraController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
+public class CameraController : Photon.PunBehaviour
 {
     // do not change variables
     [Tooltip("The Smooth time lag for the height of the camera.")]
@@ -16,13 +16,69 @@ public class CameraController : MonoBehaviour
 
     //regular variables
 
+    public Transform target;
 
+    [System.Serializable]
+    public class PositionSettings
+    {
+        public Vector3 targetPosOffset = new Vector3(0, 3.4f, 0);
+        public float lookSmooth = 100f;
+        public float distanceFromTarget = -8;
+        public float zoomSmooth = 350; //may need to be higher
+        public float maxZoom = -2;
+        public float minZoom = -15;
+        public bool smoothFollow = true;
+        public float smooth = .05f;
 
+        [HideInInspector]
+        public float newDistance = -8; //set by zoom input
+        [HideInInspector]
+        public float adjustmentDistance = -8;
+    }
 
+    [System.Serializable]
+    public class OrbitSettings
+    {
+        public float xRotation = -20;
+        public float yRotation = -180;
+        public float maxXRotation = 25;
+        public float minXRotation = -50;
+        public float vOrbitSmooth = 100f;
+        public float hOrbitSmooth = 100f;
+    }
 
+    [System.Serializable]
+    public class InputSettings
+    {
+        public string MOUSE_ORBIT = "MouseOrbit";
+        public string MOUSE_ORBIT_VERTICAL = "MouseOrbitVertical";
+        public string ORBIT_HORIZONTAL_SNAP = "OrbitHorizontalSnap";
+        public string ORBIT_HORIZONTAL = "OrbitHorizontal";
+        public string ORBIT_VERTICAL = "OrbitVertical";
+        public string ZOOM = "Mouse ScrollWheel";
+    }
 
+    [System.Serializable]
+    public class DebugSettings
+    {
+        public bool drawDesiredCollisionLines = true;
+        public bool drawAdjustedCollisionLines = true;
+    }
 
-    
+    public PositionSettings position = new PositionSettings();
+    public OrbitSettings orbit = new OrbitSettings();
+    public InputSettings input = new InputSettings();
+    public DebugSettings debug = new DebugSettings();
+    public CollisionHandler collision = new CollisionHandler();
+
+    Vector3 targetPos = Vector3.zero;
+    Vector3 destination = Vector3.zero;
+    Vector3 adjustedDestination = Vector3.zero;
+    Vector3 camVel = Vector3.zero;
+    float vOrbitInput, hOrbitInput, zoomInput, hOrbitSnapInput, mouseOrbitInput, vMouseOrbitInput;
+    Vector3 previousMousePos = Vector3.zero;
+    Vector3 currentMousePos = Vector3.zero;
+
 
 
 
@@ -35,9 +91,29 @@ public class CameraController : MonoBehaviour
         }
     }
 
+
+    void SetCameraTarget()
+    {
+        if (photonView.isMine)
+        {
+            target = this.transform;
+        }
+        else
+            Debug.Log("something went wrong. couldn't identify self transform...");
+    }
+
+    void GetInput()
+    {
+        vOrbitInput = Input.GetAxisRaw(input.ORBIT_VERTICAL);
+        hOrbitInput = Input.GetAxisRaw(input.ORBIT_HORIZONTAL);
+        hOrbitSnapInput = Input.GetAxisRaw(input.ORBIT_HORIZONTAL_SNAP);
+        zoomInput = Input.GetAxisRaw(input.ZOOM);
+    }
+
     private void Update()
     {
-
+        GetInput();
+        ZoomInOnTarget();
     }
 
     void LateUpdate()
@@ -51,7 +127,31 @@ public class CameraController : MonoBehaviour
         // only follow is explicitly declared
         if (isFollowing)
         {
-            Apply();
+            //moving
+            MoveToTarget();
+            //rotating
+            LookAtTarget();
+            //player input orbit
+            OrbitTarget();
+            MouseOrbitTarget();
+
+            collision.UpdateCameraClipPoints(cameraTransform.position, cameraTransform.rotation, ref collision.adjustedCameraClipPoints);
+            collision.UpdateCameraClipPoints(destination, cameraTransform.rotation, ref collision.desiredCameraClipPoints);
+
+            //draw debug lines
+            for (int i = 0; i < 5; i++)
+            {
+                if (debug.drawDesiredCollisionLines)
+                {
+                    Debug.DrawLine(targetPos, collision.desiredCameraClipPoints[i], Color.white);
+                }
+                if (debug.drawAdjustedCollisionLines)
+                {
+                    Debug.DrawLine(targetPos, collision.adjustedCameraClipPoints[i], Color.green);
+                }
+            }
+            collision.CheckColliding(targetPos); //using raycasts here
+            position.adjustmentDistance = collision.GetAdjustedDistanceWithRayFrom(targetPos);
         }
     }
 
@@ -59,27 +159,199 @@ public class CameraController : MonoBehaviour
     {
         cameraTransform = Camera.main.transform;
         isFollowing = true;
-        // we don't smooth anything, we go straight to the right camera shot
-        Cut();
+
+
+        SetCameraTarget();
+
+        vOrbitInput = hOrbitInput = zoomInput = hOrbitSnapInput = mouseOrbitInput = vMouseOrbitInput = 0;
+
+        MoveToTarget();
+
+        collision.Initialize(Camera.main);
+        collision.UpdateCameraClipPoints(cameraTransform.position, cameraTransform.rotation, ref collision.adjustedCameraClipPoints);
+        collision.UpdateCameraClipPoints(destination, cameraTransform.rotation, ref collision.adjustedCameraClipPoints);
+
+        previousMousePos = currentMousePos = Input.mousePosition;
     }
 
-    /// <summary>
-    /// This is what goes where LateUpdate stuff goes
-    /// </summary>
-    void Apply()
+    void MoveToTarget()
     {
+        targetPos = target.position + Vector3.up * position.targetPosOffset.y + Vector3.forward * position.targetPosOffset.z + cameraTransform.TransformDirection(Vector3.right * position.targetPosOffset.x);
+	    destination = Quaternion.Euler(orbit.xRotation, orbit.yRotation + target.eulerAngles.y, 0) * -Vector3.forward * position.distanceFromTarget;
+        destination += targetPos;
 
+        if (collision.colliding)
+        {
+            adjustedDestination = Quaternion.Euler(orbit.xRotation, orbit.yRotation + target.eulerAngles.y, 0) * Vector3.forward * position.adjustmentDistance;
+            adjustedDestination += targetPos;
+
+            if (position.smoothFollow)
+            {
+                //use smooth damp function
+                cameraTransform.position = Vector3.SmoothDamp(cameraTransform.position, adjustedDestination, ref camVel, position.smooth);
+            }
+            else
+                cameraTransform.position = adjustedDestination;
+        }
+        else
+        {
+            if (position.smoothFollow)
+            {
+                //use smooth damp function
+                cameraTransform.position = Vector3.SmoothDamp(cameraTransform.position, destination, ref camVel, position.smooth);
+            }
+            else
+                cameraTransform.position = destination;
+        }
     }
 
-    /// <summary>
-    /// Directly position the camera to a the specified Target and center.
-    /// </summary>
-    void Cut()
+    void LookAtTarget()
     {
-        float oldHeightSmooth = heightSmoothLag;
-        heightSmoothLag = 0.001f;
-        Apply();
-        heightSmoothLag = oldHeightSmooth;
+        Quaternion targetRotation = Quaternion.LookRotation(targetPos - cameraTransform.position);
+        cameraTransform.rotation = Quaternion.Lerp(cameraTransform.rotation, targetRotation, position.lookSmooth * Time.deltaTime);
+    }
+
+    void MouseOrbitTarget()
+    {
+        previousMousePos = currentMousePos = Input.mousePosition;
+    }
+
+
+    void OrbitTarget()
+    {
+        if (hOrbitSnapInput > 0)
+        {
+            orbit.yRotation = -180;
+        }
+
+        orbit.xRotation += -vOrbitInput * orbit.vOrbitSmooth * Time.deltaTime;
+        orbit.yRotation += -hOrbitInput * orbit.hOrbitSmooth * Time.deltaTime;
+
+        if (orbit.xRotation > orbit.maxXRotation)
+        {
+            orbit.xRotation = orbit.maxXRotation;
+        }
+        if (orbit.xRotation < orbit.minXRotation)
+        {
+            orbit.xRotation = orbit.minXRotation;
+        }
+    }
+
+    void ZoomInOnTarget()
+    {
+        position.distanceFromTarget += zoomInput * position.zoomSmooth * Time.deltaTime;
+
+        if (position.distanceFromTarget > position.maxZoom)
+        {
+            position.distanceFromTarget = position.maxZoom;
+        }
+        if (position.distanceFromTarget < position.minZoom)
+        {
+            position.distanceFromTarget = position.minZoom;
+        }
+    }
+
+    [System.Serializable]
+    public class CollisionHandler
+    {
+        public LayerMask collisionLayer;
+
+        [HideInInspector]
+        public bool colliding = false;
+        [HideInInspector]
+        public Vector3[] adjustedCameraClipPoints;
+        [HideInInspector]
+        public Vector3[] desiredCameraClipPoints;
+
+        Camera camera;
+
+        public void Initialize(Camera cam)
+        {
+            camera = cam;
+
+
+            adjustedCameraClipPoints = new Vector3[5];
+            desiredCameraClipPoints = new Vector3[5];
+        }
+
+        public void UpdateCameraClipPoints(Vector3 cameraPosition, Quaternion atRotation, ref Vector3[] intoArray)
+        {
+            if (!camera)
+                return;
+
+            //clear the contents of intoArray
+            intoArray = new Vector3[5];
+
+            float z = camera.nearClipPlane;
+            float x = Mathf.Tan(camera.fieldOfView / 3.41f) * z; //the float is the cushion for collision space
+
+            float y = x / camera.aspect;
+
+            //top left
+            intoArray[0] = (atRotation * new Vector3(-x, y, z)) + cameraPosition; //added and rotated the point relative to camera
+                                                                                  //top right
+            intoArray[1] = (atRotation * new Vector3(x, y, z)) + cameraPosition;
+            //bottom left
+            intoArray[2] = (atRotation * new Vector3(-x, -y, z)) + cameraPosition;
+            //bottom right
+            intoArray[3] = (atRotation * new Vector3(x, -y, z)) + cameraPosition;
+            //camera's position
+            intoArray[4] = cameraPosition - camera.transform.forward;
+        }
+
+        bool CollisionDetectedAtClipPoints(Vector3[] clipPoints, Vector3 fromPosition)
+        {
+            for (int i = 0; i < clipPoints.Length; i++)
+            {
+                Ray ray = new Ray(fromPosition, clipPoints[i] - fromPosition);
+                float distance = Vector3.Distance(clipPoints[i], fromPosition);
+                if (Physics.Raycast(ray, distance, collisionLayer))
+                {
+                    return true;
+                }
+            }
+                return false;
+        }
+
+        public float GetAdjustedDistanceWithRayFrom(Vector3 from)
+        {
+            float distance = -1;
+
+            for (int i = 0; i < desiredCameraClipPoints.Length; i++)
+            {
+                Ray ray = new Ray(from, desiredCameraClipPoints[i] - from);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if (distance == -1)
+                        distance = hit.distance;
+                    else
+                    {
+                        if (hit.distance < distance)
+                            distance = hit.distance;
+                    }
+                }
+            }
+
+            if (distance == -1)
+                return 0;
+            else
+                return distance;
+        }
+
+        public void CheckColliding(Vector3 targetPosition)
+        {
+            if (CollisionDetectedAtClipPoints(desiredCameraClipPoints, targetPosition))
+            {
+                colliding = true;
+            }
+            else
+            {
+                colliding = false;
+            }
+        }
     }
 }
+
+
 
---
+++ CasterConfineUnityProject/Assets/Scripts/Spells/SpellManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// this only holds a list of spells that we are using
+// 
+
+
+public class SpellManager : MonoBehaviour
+{
+    public List<Spell> spellList = new List<Spell>();
+
+
+
+
+	
+}
